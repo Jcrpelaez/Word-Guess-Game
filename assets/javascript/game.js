f83fdd35403a85494996fcd65a33ac21461697c5
Juan Carlos <jcrpelaez@yahoo.com>
2019-10-13 20:33:48 -0500

    added photo and arrays

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,12 @@
-var wordChoices = ["apple", "banana", "watermelon", "tacos"];
-var randomIndex = 0; //Change to be random index later, hardcoded to apple for now
+var wordChoices = ["meatballs","ikea","chocolate"];
+var randomIndex = 0; //Change to be random index later, hardcoded to meatball
 var compChoice = wordChoices[randomIndex];
-
+var selectedWord = "";
+var targetWordArray = compChoice.split(""); //Splits the word into individual letters
+var wrongLetters = [];
 console.log(compChoice); //the word the computer chose
 
-var targetWordArray = compChoice.split(""); //Splits the word into individual letters
+
 
 document.onkeyup = function(event) {
   var userLetter = event.key.toLowerCase(); //Forces all to be lowercase in our comparisons
