1aecbc246349a0dc311e84f7c2705a97861c1c77
Juan Carlos <jcrpelaez@yahoo.com>
2019-10-15 08:37:33 -0500

    added more variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,18 @@
+// Global Variable and arrays
 var wordChoices = ["meatballs","ikea","chocolate"];
 var randomIndex = 0; //Change to be random index later, hardcoded to meatball
 var compChoice = wordChoices[randomIndex];
 var selectedWord = "";
 var targetWordArray = compChoice.split(""); //Splits the word into individual letters
 var wrongLetters = [];
+var numBlanks = 0;
 console.log(compChoice); //the word the computer chose
-
-
+console.log(targetWordArray); 
+console.log(numBlanks)
+// Game counter
+var wins= 0;
+var loss= 0;
+var guessesLeft = 9;
 
 document.onkeyup = function(event) {
   var userLetter = event.key.toLowerCase(); //Forces all to be lowercase in our comparisons
