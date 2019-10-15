2058abdf300d2fcc1ab914a6885315ceb3291f6e
Juan Carlos <jcrpelaez@yahoo.com>
2019-10-15 10:02:03 -0500

    added reset and start

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,37 @@
-// Global Variable and arrays
-var wordChoices = ["meatballs","ikea","chocolate"];
-var randomIndex = 0; //Change to be random index later, hardcoded to meatball
-var compChoice = wordChoices[randomIndex];
-var selectedWord = "";
-var targetWordArray = compChoice.split(""); //Splits the word into individual letters
-var wrongLetters = [];
+// created global Variable and arrays
+var wordChoices = ["meatballs", "ikea", "chocolate"];
+var selectWord = "";
+var lettersinWord = [];
 var numBlanks = 0;
-console.log(compChoice); //the word the computer chose
-console.log(targetWordArray); 
-console.log(numBlanks)
-// Game counter
-var wins= 0;
-var loss= 0;
+var blanksAndSuccesses = []; // M _ _ _ _ _ _ _
+var wrongLetters  = [];
+console.log(wordChoices);
+console.log(lettersinWord);
+console.log(numBlanks);
+console.log(blanksAndSuccesses);
+
+
+
+var wins = 0;
+var loss = 0;
 var guessesLeft = 9;
 
-document.onkeyup = function(event) {
-  var userLetter = event.key.toLowerCase(); //Forces all to be lowercase in our comparisons
-  console.log(userLetter);
+function startGame(){
+  selectWord = wordChoices[Math.floor(Math.random() * wordChoices.length)];
+  lettersinWord = selectWord.split("");
+  numBlanks = lettersinWord.length;
 
-  // if the userLetter is in the word
-  if (targetWordArray.indexOf(userLetter) != -1) {
-    console.log("User letter is in the word!");
+
+  // Reset
+  guessesLeft = 9;
+  wrongLetters= [];
+  blanksAndSuccesses = [];
+
+  for(var i = 0; i < numBlanks; i++){
+  blanksAndSuccesses.push("_");
   }
+  
+
 };
+
+// Create variables that hold references to the places in the HTML where we want to display things.
